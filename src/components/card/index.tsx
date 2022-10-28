eecc7cb5e4e2a7350ca0893d20a59cb15929def7
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-10-28 16:51:02 +0200

    card component

---
+++ src/components/card/index.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+interface Props {
+  title: string;
+  children: React.ReactNode;
+}
+
+const Card = ({ title, children }: Props) => (
+  <div className=" shadow-lg p-4 rounded-lg flex flex-col gap-2 bg-white">
+    <div className="flex justify-between items-center">
+      <div className="font-bold">{title}</div>
+      <div className="bg-gray-200 rounded py-1 px-3" title="code">
+        <a href="https://github.com/ahmed-sudani/404-pages/tree/master/src/components/keyboard.jsx">
+          {"</>"}
+        </a>
+      </div>
+    </div>
+    <hr />
+    {children}
+  </div>
+);
+
+export default Card;
