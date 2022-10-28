cadcb11495b76bd9e6508883035bbd2f2cb0435b
ahmed-sudani <ahmed.sudani570@gmail.com>
2022-10-28 16:50:52 +0200

    keyboard component

---
+++ src/components/keyboard/index.tsx
@@ -0,0 +1,209 @@
+export default function Keyboard() {
+  return (
+    <div className="flex flex-col">
+      <div className="text-center shadow py-20 rounded-md rounded-b-none text-3xl font-Electrolize font-bold border-8 border-gray-500">
+        <p className="overflow-hidden w-fit border-r-2 border-r-gray-500 whitespace-nowrap mx-auto my-0 animate-typing p-1">
+          404 <br />
+          Page Not Found
+        </p>
+      </div>
+      <div className="rounded bg-gray-400 gap-0.5 border-8 border-gray-500 flex flex-col border-1 skew-x-30 shadow-md ml-10 w-full">
+        <div className="flex gap-0.5">
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            `
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            1
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            2
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            3
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            4
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            5
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            7
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            6
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            8
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            9
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            0
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            -
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            =
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            backspace
+          </div>
+        </div>
+        <div className="flex gap-0.5">
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            tab
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            Q
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            W
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            E
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            R
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            T
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            Y
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            U
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            I
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            O
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            P
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            [
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            ]
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            \
+          </div>
+        </div>
+        <div className="flex gap-0.5">
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            capsLock
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            A
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            S
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            D
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            F
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            G
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            H
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            J
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            K
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            L
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            ;
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            '
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            enter
+          </div>
+        </div>
+        <div className="flex gap-0.5">
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            shift
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            Z
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            X
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            C
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            V
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            B
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            N
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            M
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            ,
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            .
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            /
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            shift
+          </div>
+        </div>
+        <div className="flex gap-0.5">
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            ctrl
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            fn
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            win
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            alt
+          </div>
+          <div className="bg-white py-1 px-20 active:bg-gray-200 cursor-pointer"></div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            alt
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            ctrl
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            {"<"}{" "}
+          </div>
+          <div className="text-center bg-white py-1 basis-full px-2 text-xs active:bg-gray-200 cursor-pointer">
+            {" "}
+            {">"}{" "}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+}
